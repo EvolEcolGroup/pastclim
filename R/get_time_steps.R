a94ffe3d315a69dbf5988463b7bb10a32baaad86
Andrea Manica <am315@cam.ac.uk>
2022-08-17 15:41:24 +0100

    Full testing of get_mis_time_steps

--- R/get_time_steps.R
+++ R/get_time_steps.R
@@ -1,36 +1,36 @@
-#' Get time steps for a given MIS
+#' Get time steps for a given dataset
 #'
-#' Get the time steps available in a given dataset for a MIS.
+#' Get the time steps available in a given dataset.
 #'
-#' @param mis string giving the mis; it must use the same spelling as used in
-#' /code{mis_boundaries}
 #' @param dataset string defining dataset to be downloaded (currently only
-#' "Beyer2020" or "Krapp2021" are available)
+#' "Beyer2020", "Krapp2021" and "Example" are available). Leave it unset 
+#' if using `path_to_nc`
 #' @param path_to_nc the path to the directory containing the downloaded
 #' reconstructions. Leave it unset if you are using the companion
 #' `pastclimData` to store datasets.
 #'
 #' @export
 
-get_mis_time_steps <- function(mis, dataset = NULL, path_to_nc = NULL) {
-  if (!mis %in% mis_boundaries$mis) {
-    stop("'mis' should be one of ", paste(mis_boundaries$mis, collapse = ","))
-  }
+get_time_steps <- function(dataset = NULL, path_to_nc = NULL) {
   if (all(is.null(dataset),is.null(path_to_nc))){
-    stop("Either 'dataset' or 'path_to_nc' needs to be given")
+    stop("Either dataset or path_to_nc needs to be given")
   } else if (!any(is.null(dataset),is.null(path_to_nc))) {
-    stop("Only 'dataset' or 'path_to_nc' can be given")
+    stop("Only dataset or path_to_nc can be given")
   }
 
   if (is.null(path_to_nc)) {
     path_to_nc <- get_pastclimdata_path()
-    # we get the first available file to get info for the dataset
+  }
+  # we get the first available file to get info for the dataset
+  if (!is.null(dataset)) {
     possible_vars <- get_vars_for_dataset(dataset)
     this_file <- get_file_for_dataset(possible_vars[1], dataset)$file_name
-    path_to_nc <- file.path(path_to_nc, this_file)
+    this_file <- file.path(path_to_nc, this_file)
+  } else {
+    
   }
 
-  climate_nc <- ncdf4::nc_open(path_to_nc)
+  climate_nc <- ncdf4::nc_open(this_file)
   time_steps <- (climate_nc$dim$time$vals)
   ncdf4::nc_close(climate_nc)
   mis_time_steps <- time_steps[time_steps > (mis_boundaries[mis_boundaries$mis
--- tests/testthat/test_get_mis_time_steps.R
+++ tests/testthat/test_get_mis_time_steps.R
@@ -1,5 +1,5 @@
 test_that("get_mis_time_steps for pastclimData", {
-  skip_if(requireNamespace("pastclimData"))
+  skip_if(!requireNamespace("pastclimData"))
   expect_equal(get_mis_time_steps(mis="2",dataset="Example"),
                c(-20000,-15000), ignore_attr = TRUE)
 })
@@ -9,4 +9,12 @@ test_that("get_mis_time_steps for local file", {
                                     package = "pastclim")
   expect_equal(get_mis_time_steps(mis="2",path_to_nc = path_to_example_nc),
                c(-20000,-15000), ignore_attr = TRUE)
+})
+
+test_that("get_mis_time_steps requires correct variables", {
+  expect_error(get_mis_time_steps(mis="blah",path_to_nc = path_to_example_nc),
+               "'mis' should be one of")
+  expect_error(get_mis_time_steps(mis=2), "Either 'dataset' or ")
+  expect_error(get_mis_time_steps(mis=2, dataset = "blah", path_to_nc = "blah"),
+               "Only 'dataset' or ")
 })
