44fcbee1af5d0362f124677ed47bc8f5392d4577
Andrea Manica <am315@cam.ac.uk>
2022-09-09 10:58:53 +0100

    further reformat variables in Beyer2020
    [skip ci]

--- inst/rawdata_scripts/repackage_beyer2020_split_annual_monthly.R
+++ inst/rawdata_scripts/repackage_beyer2020_split_annual_monthly.R
@@ -2,8 +2,27 @@ library(ClimateOperators)
 library(ncdf4)
 # annual variables
 select_string <- "-select,name=biome,npp,lai,BIO1,BIO4,BIO5,BIO6,BIO7,BIO8,BIO9,BIO10,BIO11,BIO12,BIO13,BIO14,BIO15,BIO16,BIO17,BIO18,BIO19"
-cdo("-z zip_9",select_string,"Beyer2020_all_vars_v1.0.0.nc","Beyer2020_annual_vars_v1.1.0.nc")
+cdo("-z zip_9",select_string,"Beyer2020_all_vars_v1.0.0.nc","Beyer2020_annual_vars_v1.1.0_temp.nc")
+nc_in<-ncdf4::nc_open("Beyer2020_annual_vars_v1.1.0_temp.nc",write=TRUE)
+ncdf4::ncatt_put(nc_in,varid="time", attname = "units",attval = "years since 1950-01-01 00:00:00.0")
+ncdf4::ncatt_put(nc_in,varid="time", attname = "long_name",attval = "years BP")
+ncdf4::nc_close(nc_in)
+
+nc_in<-ncdf4::nc_open("Beyer2020_topography_v1.0.0.nc",write=TRUE)
+ncdf4::ncvar_put(nc_in,varid="time",vals=get_time_steps("custom","Beyer2020_annual_vars_v1.1.0_temp.nc"))
+ncdf4::ncatt_put(nc_in,varid="time", attname = "units",attval = "years since 1950-01-01 00:00:00.0")
+ncdf4::ncatt_put(nc_in,varid="time", attname = "long_name",attval = "years BP")
+ncdf4::nc_close(nc_in)
+
+cdo("-z zip_9 merge Beyer2020_annual_vars_v1.1.0_temp.nc Beyer2020_topography_v1.0.0.nc Beyer2020_annual_vars_v1.1.0.nc")
 nc_in<-ncdf4::nc_open("Beyer2020_annual_vars_v1.1.0.nc",write=TRUE)
+old_vars<-c('BIO1','BIO4','BIO5','BIO6','BIO7','BIO8','BIO9','BIO10','BIO11','BIO12','BIO13','BIO14','BIO15','BIO16','BIO17','BIO18','BIO19')
+new_vars<-c('bio01','bio04','bio05','bio06','bio07','bio08','bio09','bio10','bio11','bio12','bio13','bio14','bio15','bio16','bio17','bio18','bio19')
+nc_in<-ncdf4::nc_open("Beyer2020_annual_vars_v1.1.0.nc",write=TRUE)
+for (i in 1:length(old_vars)){
+  nc_in<-ncdf4::ncvar_rename(nc_in, old_varname = old_vars[i],
+                             new_varname = new_vars[i])
+}
 ncdf4::ncatt_put(nc_in, varid = 0, attname = "description",
                  attval = "Annual variables from Beyer et al 2020, with icesheets and internal seas removed, to be used by the R library pastclim")
 ncdf4::ncatt_put(nc_in, varid = 0, attname = "pastclim_version",
@@ -11,9 +30,6 @@ ncdf4::ncatt_put(nc_in, varid = 0, attname = "pastclim_version",
 ncdf4::ncatt_put(nc_in, varid = 0, attname = "history",
                  attval="")
 ncdf4::nc_close(nc_in)
-
-
-
 
 ######################################
 # monthly variables
@@ -49,6 +65,8 @@ ncdf4::ncatt_put(nc_in, varid = 0, attname = "pastclim_version",
                  attval="1.1.0")
 ncdf4::ncatt_put(nc_in, varid = 0, attname = "history",
                  attval="")
+ncdf4::ncatt_put(nc_in,varid="time", attname = "units",attval = "years since 1950-01-01 00:00:00.0")
+ncdf4::ncatt_put(nc_in,varid="time", attname = "long_name",attval = "years BP")
 ncdf4::nc_close(nc_in)
 unlink("Beyer2020_monthly0*")
 unlink("Beyer2020_monthly_vars_temp.nc")
