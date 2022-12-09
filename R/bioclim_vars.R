269bfa1a6a28d7402dc5a95c60d93848eaf46c9a
Andrea Manica <am315@cam.ac.uk>
2022-12-09 16:59:21 +0000

    First implementation of bioclim_vars

---
+++ R/bioclim_vars.R
@@ -0,0 +1,207 @@
+#' Compute the BIOCLIM variables
+#' 
+#' To compute BIOCLIM variables from 
+#' monthly average temperature and precipitation data. For modern data,
+#' this variables are generally computed using min and maximum temperature,
+#' but for many paleoclimatic reconstructions one average temperature is
+#' avaliable. Most variables, with the exception of BIO02 and BIO03, can
+#' be rephrased meaningfully in terms of mean temperature. 
+#' This function is a modified version of \code{predicts::bcvars}.
+#' The variables are:
+#' BIO01 = Annual Mean Temperature
+#' BIO04 = Temperature Seasonality (standard deviation *100)
+#' BIO05 = Max Temperature of Warmest Month
+#' BIO06 = Min Temperature of Coldest Month
+#' BIO07 = Temperature Annual Range (P5-P6)
+#' BIO08 = Mean Temperature of Wettest Quarter
+#' BIO09 = Mean Temperature of Driest Quarter
+#' BIO10 = Mean Temperature of Warmest Quarter
+#' BIO11 = Mean Temperature of Coldest Quarter
+#' BIO12 = Annual Precipitation
+#' BIO13 = Precipitation of Wettest Month
+#' BIO14 = Precipitation of Driest Month
+#' BIO15 = Precipitation Seasonality (Coefficient of Variation)
+#' BIO16 = Precipitation of Wettest Quarter
+#' BIO17 = Precipitation of Driest Quarter
+#' BIO18 = Precipitation of Warmest Quarter
+#' BIO19 = Precipitation of Coldest Quarter
+#' 
+#' These summary Bioclimatic variables are after:
+#' Nix, 1986. A biogeographic analysis of Australian elapid snakes. In: R. Longmore (ed.).
+#'     Atlas of elapid snakes of Australia. Australian Flora and Fauna Series 7.
+#'     Australian Government Publishing Service, Canberra.
+#' 
+# and Expanded following the ANUCLIM manual
+#'
+#'
+#' @param tavg monthly average temperatures, as a matrix or 
+#' a \code{terra::SpatRaster} 
+#' @param prec monthly precipitation, as a matrix or 
+#' a \code{terra::SpatRaster} 
+#' @param ... additional variables for specific methods
+#' @returns an array or a \code{terra::SpatRaster} where each layer 
+#' is a bioclim variable
+#' @docType methods
+#' @rdname bioclim_vars-methods
+#' @importFrom methods setGeneric
+#' @export
+
+#if (!isGeneric("bioclim_vars")) {
+methods::setGeneric("bioclim_vars", function(prec, tavg, ...){
+  methods::standardGeneric("bioclim_vars")
+})
+
+#' @rdname bioclim_vars-methods
+#' @export
+methods::setMethod("bioclim_vars", signature(prec="numeric", tavg="numeric"), 
+          function(prec, tavg) {
+            bioclim_vars(t(as.matrix(prec)), t(as.matrix(tavg)))
+          }
+)
+
+#' @param filename filename where the raster can be stored.
+#' @rdname bioclim_vars-methods
+#' @export
+methods::setMethod("bioclim_vars", signature(prec="SpatRaster", tavg="SpatRaster"), 
+          function(prec, tavg, filename="", ...) {
+            
+            if (nlyr(prec) != 12) stop("nlyr(prec) is not 12")
+            if (nlyr(tavg) != 12) stop("nlyr(tavg) is not 12")
+
+            
+            x <- c(prec, tavg)
+            readStart(x)
+            on.exit(readStop(x))
+            nc <- ncol(x)
+            out <- rast(prec, nlyr=17)
+            names(out) = c("bio01",paste0("bio0", 4:9),
+                            paste0("bio",10:19))
+            b <- writeStart(out, filename, ...)
+            for (i in 1:b$n) {
+              d <- readValues(x, b$row[i], b$nrows[i], 1, nc, TRUE, FALSE)
+              p <- bioclim_vars(d[,1:12], d[,13:24])
+              writeValues(out, p, b$row[i], b$nrows[i])
+            }
+            writeStop(out)
+            return(out)
+          }
+)
+
+
+#' @rdname bioclim_vars-methods
+#' @export
+methods::setMethod("bioclim_vars", signature(prec="matrix", tavg="matrix"), 
+          function(prec, tavg) {
+            
+            if (nrow(prec) != nrow(tavg) ) {
+              stop("prec and tavg should have same length")
+            }
+            
+            if (ncol(prec) != ncol(tavg)) {
+              stop("prec and tavg should have same number of variables (columns)")
+            }
+            
+            # can"t have missing values in a row
+            nas <- apply(prec, 1, function(x){ any(is.na(x)) } )
+            nas <- nas | apply(tavg, 1, function(x){ any(is.na(x)) } )
+            p <- matrix(nrow=nrow(prec), ncol=17)
+            colnames(p) = c("bio01",paste0("bio0", 4:9),
+                            paste0("bio",10:19))
+            if (all(nas)) { return(p) }
+            
+            prec[nas,] <- NA
+            tavg[nas,] <- NA
+
+            window <- function(x)  { 
+              lng <- length(x)
+              x <- c(x,  x[1:3])
+              m <- matrix(ncol=3, nrow=lng)
+              for (i in 1:3) { m[,i] <- x[i:(lng+i-1)] }
+              apply(m, MARGIN=1, FUN=sum)
+            }
+
+            # P1. Annual Mean Temperature 
+            p[,"bio01"] <- apply(tavg,1,mean)
+            # P4. Temperature Seasonality (standard deviation) 
+            p[,"bio04"] <- 100 * apply(tavg, 1, stats::sd)
+            # P5. Max Temperature of Warmest Period 
+            p[,"bio05"] <- apply(tavg,1, max)
+            # P6. Min Temperature of Coldest Period 
+            p[,"bio06"] <- apply(tavg, 1, min)
+            # P7. Temperature Annual Range (P5-P6) 
+            p[,"bio07"] <- p[,"bio05"] - p[,"bio06"]
+            # P12. Annual Precipitation 
+            p[,"bio12"] <- apply(prec, 1, sum)
+            # P13. Precipitation of Wettest Period 
+            p[,"bio13"] <-  apply(prec, 1, max)
+            # P14. Precipitation of Driest Period 
+            p[,"bio14"] <-  apply(prec, 1, min)
+            # P15. Precipitation Seasonality(Coefficient of Variation) 
+            # the "1 +" is to avoid strange CVs for areas where mean rainfaill is < 1)
+            p[,"bio15"] <- apply(prec+1, 1, .cv)
+            
+            # precip by quarter (3 months)		
+            wet <- t(apply(prec, 1, window))
+            # P16. Precipitation of Wettest Quarter 
+            p[,"bio16"] <- apply(wet, 1, max)
+            # P17. Precipitation of Driest Quarter 
+            p[,"bio17"] <- apply(wet, 1, min)
+            tmp <- t(apply(tavg, 1, window)) / 3
+            
+            if (all(is.na(wet))) {
+              p[,"bio08"] <- NA		
+              p[,"bio09"] <- NA		
+            } else {
+              # P8. Mean Temperature of Wettest Quarter 
+              wetqrt <- cbind(1:nrow(p), as.integer(apply(wet, 1, which.max)))
+              p[,"bio08"] <- tmp[wetqrt]
+              # P9. Mean Temperature of Driest Quarter 
+              dryqrt <- cbind(1:nrow(p), as.integer(apply(wet, 1, which.min)))
+              p[,"bio09"] <- tmp[dryqrt]
+            }
+            # P10 Mean Temperature of Warmest Quarter 
+            p[,"bio10"] <- apply(tmp, 1, max)
+            
+            # P11 Mean Temperature of Coldest Quarter
+            p[,"bio11"] <- apply(tmp, 1, min) 
+            
+            if (all(is.na(tmp))) {
+              p[,"bio18"] <- NA		
+              p[,"bio19"] <- NA
+            } else {
+              # P18. Precipitation of Warmest Quarter 
+              hot <- cbind(1:nrow(p), as.integer(apply(tmp, 1, which.max)))
+              p[,"bio18"] <- wet[hot]
+              # P19. Precipitation of Coldest Quarter 
+              cold <- cbind(1:nrow(p), as.integer(apply(tmp, 1, which.min)))
+              p[,"bio19"] <- wet[cold]
+            }
+            
+            return(p)	
+          }
+)
+
+#' Coefficient of variables (expressed as percentage)
+#' 
+#' R function to compute the coefficient of variation 
+#' (expressed as a percentage). If there is only a single value, stats::sd = NA. 
+#' However, one could argue that cv =0; and NA may break the code that
+#' receives it. The function returns 0 if the mean is close to zero.
+#' 
+#' This is ODD: abs to avoid very small (or zero) mean with e.g. -5:5
+#' 
+#' @param x a vector of values
+#' @returns the cv
+#' @keywords internal
+
+.cv <- function(x) {
+  m <- mean(abs(x))
+  if (is.na(m)){return(NA)}
+  if (m == 0) {
+    return(0)
+  } else {
+    return(100 * stats::sd(x) / m)
+  }
+}
+
+  
--- inst/temp/monthly_vars.R
+++ inst/temp/monthly_vars.R
@@ -1,16 +1,40 @@
-library(geodata)
+# the code below downloads the full worldclim dataset. Another option is to use
+# worldclim_tile("tavg",lon=0,lat=52,path=tempdir())
+# to only get part of the world
+
 path_wc <- "../../project_temp/climate_downscale/worldclim_monthly"
-wc_tavg_5<-worldclim_global(var="tavg", res=5, path=path_wc, version="2.1")
-# to read in again the tiffs
-path_wc <- file.path(path_wc,"wc2.1_5m")
-list_of_tiffs <- file.path(path_wc,dir(path_wc))
-wc_tavg_5<-terra::rast(list_of_tiffs)
-# better to save it as a netcdf
-writeCDF(wc_tavg_5, file.path(get_data_path(),"wc2.1_5m_tavg.nc"))
+wc_res <- 5 # one of 10,5,2.5,0.5 minutes
 
-One month
-foo<-wc_tavg_5[[1]]
+library(geodata)
+library(pastclim)
 
+# load the observations (and download them if needed)
+if (!file.exists(file.path(base_path_wc,"wc_tavg_5_monthly.nc"))){
+  wc_tavg_5<-geodata::worldclim_global(var="tavg", res=wc_res, path=tempdir(), version="2.1")
+  terra::writeCDF(wc_tavg_5,file.path(base_path_wc,"wc_tavg_5_monthly.nc"),
+                  compression=9)
+} else {
+  wc_tavg_5<-terra::rast(file.path(base_path_wc,"wc_tavg_5_monthly.nc"))
+}
 
-##get a tile
-foo<-worldclim_tile("tavg",lon=0,lat=52,path=tempdir())
+if (!file.exists(file.path(base_path_wc,"wc_prec_5_monthly.nc"))){
+  wc_prec_5<-geodata::worldclim_global(var="prec", res=wc_res, path=tempdir(), version="2.1")
+  terra::writeCDF(wc_prec_5,file.path(base_path_wc,"wc_prec_5_monthly.nc"),
+                  compression=9)
+} else {
+  wc_prec_5<-terra::rast(file.path(base_path_wc,"wc_prec_5_monthly.nc"))
+}
+
+# now set the extent for SEA
+sea_ext<- terra::ext(100, 110, 0, 10)
+# crop the observations for this extent
+high_res_tavg <- terra::crop(wc_tavg_5, sea_ext)
+high_res_prec <- terra::crop(wc_prec_5, sea_ext)
+
+bioclim_pres <- bioclim_vars(tavg = high_res_tavg, prec = high_res_prec)
+
+tavg_series <- region_series(bio_variables = c(paste0("temperature_0",1:9),paste0("temperature_",10:12)),
+                             time_bp =  get_time_steps(dataset = "Example"),
+                             dataset = "Beyer2020",
+                              ext = sea_ext)
+
