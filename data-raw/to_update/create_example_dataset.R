06fcd442d93ccde62008da113fa4957873d0aa43
Andrea Manica <am315@cam.ac.uk>
2023-04-18 16:38:40 +0100

    fix ice sheets in example dataset

--- data-raw/to_update/create_example_dataset.R
+++ data-raw/to_update/create_example_dataset.R
@@ -1,3 +1,6 @@
+file_version <- "1.3.0"
+file_name<-paste0("example_climate_v", file_version,".nc")
+
 setwd(tempdir())
 library(ClimateOperators)
 library(pastclim)
@@ -7,37 +10,45 @@ this_file <- file.path(get_data_path(), this_file)
 
 
 ## subset to a few variables and time steps
-cdo (paste0("select,name=bio01,bio10,bio12,biome ",this_file," example_climate_all_time.nc"))
-ncks ("-O -d time,51,71,5 example_climate_all_time.nc example_climate_subset.nc")
-ncatted ('-O -a description,global,m,c,"Sample dataset to be used in pastclim, a subset of Beyer2020" -h example_climate_subset.nc')
-ncatted ('-a command_line,global,c,c,"./inst/rawdata_scripts/created_example_dataset.sh" -h example_climate_subset.nc')
+cdo (paste0("select,name=bio01,bio10,bio12,biome ",this_file," ex_all_time.nc"))
+ncks ("-O -d time,51,71,5 ex_all_time.nc ex_subset.nc")
+ncatted ('-O -a description,global,m,c,"Sample dataset to be used in pastclim, a subset of Beyer2020" -h ex_subset.nc')
 
 # now upscale the bio variables
-cdo("select,name=bio01,bio10,bio12 example_climate_subset.nc example_climate_bio.nc")
-cdo("gridboxmean,2,2 example_climate_bio.nc example_climate_bio_mean.nc")
+cdo("select,name=bio01,bio10,bio12 ex_subset.nc ex_bio.nc")
+cdo("gridboxmean,2,2 ex_bio.nc ex_bio_mean.nc")
 # upscale the biomes (we need to make sure that the ice and ocean masks match the observations)
-cdo("select,name=biome example_climate_subset.nc example_climate_biome.nc")
+cdo("select,name=biome ex_subset.nc ex_biome.nc")
 # this mask is correct for the ocean mask
-cdo("gridboxmax,2,2 example_climate_biome.nc example_climate_biome_max.nc")
+cdo("gridboxmax,2,2 ex_biome.nc ex_biome_max.nc")
 # this mask is correct for the ice mask
-cdo("gridboxmin,2,2 example_climate_biome.nc example_climate_biome_min.nc")
-
-## CHECK the following
-
+cdo("gridboxmin,2,2 ex_biome.nc ex_biome_min.nc")
 # create an ice mask with the correct ice
-cdo ("-O -expr,'biome = ((biome < 28)) ? biome : -1' example_climate_biome_min.nc ice1.nc")
-cdo ("-O -expr,'biome = ((biome >= 28)) ? 1 : 0' ice1.nc ice2.nc")
+cdo ("-O -expr,'biome = ((biome < 28)) ? 0 : -1' ex_biome_min.nc ice1.nc")
 # create mask of ice that is too large
-cdo ("-O -expr,'biome = ((biome < 29)) ? biome : -1' example_climate_biome_max.nc land1.nc")
-cdo ("-O -expr,'biome = ((biome >= 0)) ? 1 : 0' land1.nc land2.nc")
+cdo ("-O -expr,'biome = ((biome == 28)) ? 1 : 0' ex_biome_max.nc ice_big.nc")
+# get the difference
+cdo ("-O add ice1.nc ice_big.nc ice_to_remove.nc")
+cdo ("-O chname,biome,to_remove ice_to_remove.nc ice_to_remove_rename.nc")
+cdo ("-O merge ex_biome_max.nc ice_to_remove_rename.nc ex_biome_temp.nc")
+cdo ("-O -expr,'biome = ((to_remove == 1)) ? 27 : biome' ex_biome_temp.nc ex_biome_correct.nc")
+# extract the new biome
+cdo("select,name=biome ex_biome_correct.nc ex_biome_only.nc")
+# copy all attributes (which were lost) to the file
+attr_src <- ncdf4::nc_open("ex_all_time.nc")
+attr_dest <- ncdf4::nc_open("ex_biome_only.nc",write=TRUE)
+ncdf4.helpers::nc.copy.atts(attr_src,"biome",attr_dest,"biome",
+                            exception.list = c("_FillValue"))
+ncdf4::nc_close(attr_src)
+ncdf4::nc_close(attr_dest)
 
-cdo ("add ice2.nc ice_big2.nc ice_to_remove.nc")
+# change the names (we use it to test the ability to convert names if needed)
+cdo("-O chname,bio01,BIO1,bio10,BIO10,bio12,BIO12","ex_bio_mean.nc","ex_bio_mean_rename.nc")
 
+cdo("-O -z zip_9 merge ex_bio_mean_rename.nc ex_biome_only.nc",file_name)
 
-
-cdo("select,name=bio01,bio10,bio12 example_climate_mean.nc example_climate_small3.nc")
-
-cdo("gridboxmin,2,2 example_climate_subset.nc example_climate_small5.nc")
-
-
-cdo("-z zip_9 merge example_climate_small3.nc example_climate_small4.nc example_climate_small5.nc")
+ncatted ('-a command_line,global,m,c,"./inst/rawdata_scripts/created_example_dataset.R" -h', file_name)
+ncatted ('-a history,global,m,c,"" -h', file_name)
+ncatted (paste0('-a pastclim_version,global,m,c,"',file_version,'" -h'), file_name)
+cdo("-O -z zip_9 merge ex_bio_mean.nc ex_biome_only.nc","example_to_rename.nc")
+file.copy(file_name,to = "~/git/pastclim/inst/extdata/", overwrite = TRUE)
