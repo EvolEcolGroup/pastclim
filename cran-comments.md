a9a8c62d013f79743a3e6632029da2dedce4cd8c
Andrea Manica <am315@cam.ac.uk>
2023-04-25 17:05:53 +0100

    Final changes before submission

--- cran-comments.md
+++ cran-comments.md
@@ -4,21 +4,4 @@
 - R-hub fedora-clang-devel (r-devel)
 - devtools::check_mac_release
 
-On all testing environment (see below), I only get a note because a DOI from
-a Wiley publication which is flagged as possibly invalid. The DOI is valid,
-but the Wiley server turns down automatic requests (as discussed here https://hypatia.math.ethz.ch/pipermail/r-package-devel/2022q2/008043.html)
-
-## R CMD check results
-On all systems:
-
-* checking CRAN incoming feasibility ... NOTE
-Maintainer: ‘Andrea Manica <am315@cam.ac.uk>’
-
-  Found the following (possibly) invalid URLs:
-    URL: https://doi.org/10.1111/ecog.06481
-      From: man/pastclim.Rd
-            README.md
-      Status: 403
-      Message: Forbidden
-
-0 errors ✔ | 0 warnings ✔ | 1 notes ✖
+No NOTES on any environment.
--- data-raw/helper_functions/submitting_to_cran.R
+++ data-raw/helper_functions/submitting_to_cran.R
@@ -1,3 +1,4 @@
+# to this in the dev branch to make sure that all the fundamental issues have been resolved
 # run the spell checking
 usethis::use_spell_check(lang="en-GB")
 
@@ -9,6 +10,12 @@ devtools::check(remote = TRUE, manual = TRUE)
 
 source("./data-raw/helper_functions/check_returns_in_documentation.R")
 
+# now create a cran_submission branch for the final remote tests
+
+# TO DO MANUALLY: update cran-comments.md accordingly
+# update version number
+# update news
+
 # first check with rhub
 pastclim_checks <- rhub::check_for_cran()
 
@@ -16,9 +23,5 @@ pastclim_checks <- rhub::check_for_cran()
 devtools::check_mac_release()
 devtools::check_win_devel()
 
-# TO DO MANUALLY: update cran-comments.md accordingly
-# upadte version number
-# update news
-
-# if everything passes, edit the cran-comments.md to explain any notes
+# TO DO MANUALLY: if everything passes, edit the cran-comments.md to explain any notes
 devtools::release()
