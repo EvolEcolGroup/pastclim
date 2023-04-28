d12bd35469af0e9a194b1f1ca3971b46cb5c9d79
Andrea Manica <am315@cam.ac.uk>
2023-04-28 11:03:27 +0100

    make some more functions work with series
    get_land_mask, get_ice_maks and distance_from_sea now work on series rather htan just a single slice

--- R/distance_from_sea.R
+++ R/distance_from_sea.R
@@ -7,22 +7,44 @@
 #' @param dataset string defining the dataset to use (a list of possible
 #' values can be obtained with [get_available_datasets()]). This function
 #' will not work on custom datasets.
-#' @returns a [`terra::SpatRaster`] of distances
+#' @returns a [`terra::SpatRaster`] of distances from the coastline in km
 #'
 #' @import terra
 #' @export
 
-distance_from_sea <- function(time_bp, dataset) {
-  this_land_mask <- get_land_mask(time_bp=time_bp,
-                                            dataset=dataset)
-  coastlines_polyline <- terra::as.polygons(this_land_mask) # first get polygon
-  coastlines_polyline <- terra::as.lines(coastlines_polyline) # then extract line
-  
-  distances_rast <- terra::distance(this_land_mask$land_mask, 
-                                    coastlines_polyline,
-                                    unit="km")
-  distances_rast <- terra::mask(distances_rast, this_land_mask)
-  names(distances_rast) <- "distance_from_sea"
-  
-  return(distances_rast)
+distance_from_sea <- function(time_bp=NULL, dataset) {
+  times <- get_time_steps(dataset = dataset)
+  # if we don't give times, just use all available
+  if (is.null(time_bp)){
+    time_bp <- times
+  }
+  # we use this to correctly convert time_bp
+  time_index <- time_bp_to_i_series(time_bp = time_bp,
+                                    time_steps = times)
+  distances_list <- list()
+  for (i in 1:length(time_index)){
+    this_time <- times[time_index[i]]
+    this_land_mask <- get_land_mask(time_bp=this_time,
+                                    dataset=dataset)
+    coastlines_with_ice <- region_slice(time_bp=this_time,
+                                        bio_variables="biome",
+                                        dataset=dataset)
+    coastlines_with_ice[coastlines_with_ice> -1]<-1
+    coastlines_polyline <- terra::as.polygons(coastlines_with_ice) # first get polygon
+    coastlines_polyline <- terra::as.lines(coastlines_polyline) # then extract line
+    
+    distances_rast <- terra::distance(this_land_mask[[1]], 
+                                      coastlines_polyline,
+                                      unit="km")
+    distances_rast <- terra::mask(distances_rast, this_land_mask)
+    #browser()
+    names(distances_rast) <- paste("distance_from_sea",time_bp(distances_rast),sep="_")
+    if (i==1){
+      distances_all <- distances_rast
+    } else {
+      distances_all <- c(distances_all, distances_rast)
+    }
+  }
+  varnames(distances_all) <- "distance_from_sea"
+  return(distances_all)
 }
