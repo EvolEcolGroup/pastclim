3712d900b418b5cb267770ed8bbf641ba224370f
Andrea Manica <am315@cam.ac.uk>
2023-07-09 20:46:59 +0100

    Avoid unloading sf

--- R/download_worldclim_present.R
+++ R/download_worldclim_present.R
@@ -70,13 +70,6 @@ download_worldclim_present <- function(dataset, bio_var, filename){
   # and finally we save it as a netcdf file
   time_bp(wc_rast) <- rep(35,nlyr(wc_rast))
   
-  # HACK
-  # temporary workaround to prevent problems with sf being loaded whilst writing netcdf
-  sf_load <- FALSE
-  if (isNamespaceLoaded("sf")){
-    unloadNamespace("sf")
-  }
-  
   terra::writeCDF(wc_rast,filename=filename, compression=9, 
                   split=TRUE, overwrite=TRUE)
   
@@ -84,11 +77,7 @@ download_worldclim_present <- function(dataset, bio_var, filename){
   nc_in <- ncdf4::nc_open(filename, write=TRUE)
   ncdf4::ncatt_put(nc_in, varid="time", attname="axis", attval = "T")
   ncdf4::nc_close(nc_in)
-  if (sf_load){ # reload the namespace if it was there to start with
-    loadNamespace("sf")
-    attachNamespace("sf")
-  }
-  
+
   # clean up
   unlink(file.path(destpath,"*"))
 }
