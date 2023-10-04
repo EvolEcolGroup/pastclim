7285330f378f4fc292a68bf286e1eb0dffdfd1a3
Andrea Manica <am315@cam.ac.uk>
2023-10-04 21:17:49 +0100

    first draft of script for krapp2021

---
+++ data-raw/to_update/repackage_krapp2021.R
@@ -0,0 +1,70 @@
+# This script repackages the Krapp dataset, and creates an annual and monthly set
+# of variables. It generates both a "full" dataset with the ice sheets, and a "land"
+# dataset with land only (which is the default for pastclim)
+
+# it can be simply run with something like
+# nohup Rscript "~/git/pastclim/data-raw/to_update/repackage_krapp2020 rewrite.R" > repackage_krapp2020.log 2>&1 & 
+# nohup Rscript "repackage_krapp2020 rewrite.R" > repackage_krapp2020.log 2>&1 & 
+
+
+# ideally set up a conda environment before running this script
+# conda create --name krapp_repackage
+# conda activate krapp_repackage
+# conda install -c conda-forge mamba
+# mamba install -c conda-forge cdo
+
+library(reticulate)
+use_condaenv("beyer_repackage", required= FALSE)
+
+# TO UPDATE MANUALLY
+file_version <- "1.4.0"
+# working directory where the final files will be stored
+wkdir <- "/media/andrea/Elements/pastclim/"
+
+# everything below here should work automatically
+library(ClimateOperators)
+library(ncdf4)
+
+setwd(wkdir)
+
+#output file names
+uncut_ann_filename <- paste0("Krapp2021_uncut_annual_vars_v",file_version,".nc")
+uncut_month_filename <- paste0("Krapp2021_uncut_monthly_vars_v",file_version,".nc")
+ann_filename <- paste0("Krapp2021_annual_vars_v",file_version,".nc")
+month_filename <- paste0("Krapp2021_monthly_vars_v",file_version,".nc")
+
+# names of directory with original files
+download_dir <- file.path(wkdir,"original_files")
+if (!dir.exists("./repackaged")){
+  dir.create("./repackaged")
+}
+# create landmask
+cdo("select,name=biome ./original_files/biome4output_800ka.nc krapp_biome.nc")
+cdo ("-O -expr,'biome = ((biome < 28)) ? biome : -1' krapp_biome.nc krapp_biome2.nc")
+cdo ("-O -expr,'biome = ((biome >= 0)) ? 1 : 0' krapp_biome2.nc krapp_land_only.nc")
+file.remove("krapp_biome.nc", "krapp_biome2.nc")
+
+# now process each file
+for (file_id in list.files(download_dir,full.names = TRUE)){
+  this_file <- basename(file_id)
+  var1 <- strsplit(this_file,"_")[[1]][1]
+  if (var1!="biome4output"){
+    new_file <- file.path("./repackaged", paste0("Krapp2021_",var1,"_v",file_version,".nc"))
+
+    cdo(paste0("-z zip_9 div ",file_id," krapp_land_only.nc ",new_file))
+    
+  } else {
+    ##TODO this has not been implemented yet!!!!
+    #new_file <- file.path("./repackaged/", paste0(var1,"_biome_v",file_version,".nc"))
+    #ncks(paste0("-C -O -x -v pco2,npp,LAI ", file_id, " ", new_file))
+    
+  }
+}
+
+for (file_id in list.files("./repackaged", full.names=TRUE)){
+  ncatted (paste0("-a author,global,d,, -a history,global,d,, -a description,global,d,, -a command_line,global,d,, -h ",file_id))
+ncatted (paste0("-a created_by,global,c,c,'Andrea Manica' -a pastclim_version,global,c,c,'",file_version,"' ",file_id))
+ncatted (paste0("-a link,global,a,c,'https://github.com/EvolEcolGroup/pastclim' -a description,global,a,c,'Data from Krapp et al 2021, with icesheets and internal seas removed, to be used by the R library pastclim' -a history,global,d,, -a history_of_appended_files,global,d,, -h ",file_id))
+ncatted (paste0("-a units,time,m,c,'years since present' -h ",file_id))
+ncatted (paste0("-a command_line,global,c,c,'./inst/rawdata_scripts/repackage_krapp2021.R' -h ",file_id))
+}
