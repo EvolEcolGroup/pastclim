d4b2279c83a468c9180bcc0bb2ecd619abf9212f
Andrea Manica <am315@cam.ac.uk>
2024-01-23 08:10:42 +0000

    fix chelsa download for vsi

--- R/download_chelsa_present.R
+++ R/download_chelsa_present.R
@@ -3,13 +3,13 @@
 #' This function downloads annual and monthly variables from the CHELSA v2.1 dataset.
 #' @param dataset the name of the dataset
 #' @param bio_var the variable name
-#' @param filename the filename as stored in the `data_path` of `pastclim`
+#' @param filename the filename as stored in the `data_path` of `pastclim` (includes the full data path)
 #' @returns TRUE if the requested CHELSA variable was downloaded successfully
 #' 
 #' @keywords internal
 
 
-# download_chelsa_present(dataset = "CHELSA_2.1_0.5m_vsi", bio_var = "bio_06",
+# download_chelsa_present(dataset = "CHELSA_2.1_0.5m_vsi", bio_var = "bio06",
 # filename = "CHELSA_2.1_0.5m_bio06_vsi.vrt")
 
 download_chelsa_present <- function(dataset, bio_var, filename) {
@@ -20,7 +20,6 @@ download_chelsa_present <- function(dataset, bio_var, filename) {
   } else {
     virtual <- FALSE
   }
-
   # compose download paths
   download_url <- filenames_chelsa_present(dataset=dataset,
                                            bio_var = bio_var)
@@ -29,7 +28,6 @@ download_chelsa_present <- function(dataset, bio_var, filename) {
     # add prefix for vsi dataset
     chelsa_url <- paste0("/vsicurl/", download_url) # urls of target files
   } else { # download the files
-    stop("not implemented yet!")
     # if we do not have a directory, create one
     chelsa_dir <- file.path(get_data_path(),"chelsa_2.1")
     if(!dir.exists(chelsa_dir)){
@@ -42,12 +40,12 @@ download_chelsa_present <- function(dataset, bio_var, filename) {
                         quiet = FALSE
     )
   }
-  browser()
   # create the vrt file
-  # TODO we shoudl capture a warning here and abort
+  # TODO we should capture a warning here and abort
   vrt_path <- terra::vrt(x = chelsa_url,
-                         filename = file.path(get_data_path(),filename),
-                         options="-separate", overwrite=TRUE, return_filename=TRUE)
+                         filename = filename,
+                         #options="-separate", 
+                         overwrite=TRUE, return_filename=TRUE)
 
   # edit the vrt metadata
   return(vrt_set_meta(vrt_path = vrt_path, description = bio_var,
