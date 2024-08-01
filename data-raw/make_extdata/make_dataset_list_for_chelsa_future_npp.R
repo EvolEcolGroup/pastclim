b0fd0fa084930bec78568aa573e33752b7df17ae
Andrea Manica <am315@cam.ac.uk>
2024-08-01 12:42:21 +0100

    add npp for chelsa present and future

---
+++ data-raw/make_extdata/make_dataset_list_for_chelsa_future_npp.R
@@ -0,0 +1,35 @@
+# start this script from this directory
+chelsa_template <- read.csv("../data_files/chelsa_future_template_npp.csv")
+gcm <- c(
+  "GFDL-ESM4","IPSL-CM6A-LR", "MPI-ESM1-2-HR","MRI-ESM2-0","UKESM1-0-LL"
+)
+scenarios <- c("ssp126","ssp370","ssp585")
+
+resolutions <- c(0.5) # add 2.5 and 0.5
+library(dplyr)
+library(stringr)
+chelsa_future <- chelsa_template[0, ]
+for (i_gcm in gcm) {
+  for (i_scenario in scenarios) {
+    for (i_res in resolutions) {
+      chelsa_this <- chelsa_template %>%
+        mutate(
+          dataset = str_replace(dataset, "XXX", i_gcm),
+          file_name = str_replace(file_name, "XXX", i_gcm)
+        ) %>%
+        mutate(
+          dataset = str_replace(dataset, "YYY", i_scenario),
+          file_name = str_replace(file_name, "YYY", i_scenario)
+        )
+      #%>%
+      #  mutate(
+      #    dataset = str_replace(dataset, "ZZZ", as.character(i_res)),
+      #    file_name = str_replace(file_name, "ZZZ", as.character(i_res))
+      #  )
+      chelsa_future <- chelsa_future %>% bind_rows(chelsa_this)
+    }
+  }
+}
+write.csv(chelsa_future, "../data_files/chelsa_future_list_npp.csv",
+  row.names = FALSE, na = ""
+)
