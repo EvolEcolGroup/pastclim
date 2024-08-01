305d2e36ec9c194f144bf8e9f021c68304eeaaf3
Andrea Manica <am315@cam.ac.uk>
2024-08-01 11:17:49 +0100

    create filenames for npp for present and future

--- R/filenames_chelsa_present.R
+++ R/filenames_chelsa_present.R
@@ -13,6 +13,9 @@ filenames_chelsa_present <- function(dataset, bio_var){
   if ("bio"== substr(bio_var,1,3)){
     var_prefix <- "bio"
     var_index <- paste0(var_prefix,as.numeric(var_index)) # strip leading 0
+  } else if (length(grep(pattern="npp",bio_var))){  
+    var_prefix <- "bio"
+    var_index <- "npp"  
   } else if (length(grep(pattern="temperature_min",bio_var))){
     var_prefix <- "tasmin"
     var_index <- paste0(var_prefix,"_",var_index)        
