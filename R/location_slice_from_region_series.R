4a1743ac6a34d46118f6cbdafd1c49f410101954
Andrea Manica <am315@cam.ac.uk>
2024-09-20 09:34:02 +0100

    Revert "Improve error handling of dataset names in region_series"

--- R/location_slice_from_region_series.R
+++ R/location_slice_from_region_series.R
@@ -52,10 +52,6 @@ location_slice_from_region_series <-
            directions = 8) {
     # get the region series for this dataset
     climate_brick <- region_series
-    # Check that region_series has valid names
-    if (is.null(names(region_series)) || any(make.names(names(region_series)) != names(region_series))) {
-      stop("The subdatasets in 'region_series' must have valid names.")
-    }
     bio_variables <- names(region_series)
 
     time_bp <- check_time_vars(time_bp = time_bp, time_ce = time_ce)
