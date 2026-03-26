77e2a02254af35f7ea429fc67a6e3993f0da899b
Andrea Manica <am315@cam.ac.uk>
2026-03-26 16:29:50 +0000

    Update docs

--- R/datasets_docs.R
+++ R/datasets_docs.R
@@ -339,6 +339,10 @@ NULL
 #' precipitation data since the Last Glacial Maximum. Climate of the Past.
 #' \doi{10.5194/cp-2021-30}
 #'
+#'
+#' @note There is a missing time slice at -130 years BP for `temperature_min_07`
+#' and `temperature_max_07` on the CHELSA server. That timestep is replaced with
+#' -120 years BP in `pastclim` for those two variables.
 #' @name CHELSA_trace21k_1.0
 NULL
 #> NULL
